0bb3f53a23a61919ba01cc7f9a64e94e99b10bee
martin <martygrof3708@gmail.com>
2024-03-12 04:39:02 -0400

    AVALONIA: Changed content view selection to top menu.

--- dlTubeAvalonia/dlTubeAvalonia/Views/MainWindow.axaml.cs
+++ dlTubeAvalonia/dlTubeAvalonia/Views/MainWindow.axaml.cs
@@ -1,11 +1,27 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace dlTubeAvalonia.Views;
 
 public partial class MainWindow : Window
 {
+    readonly YoutubeView _youtubeView;
+    ArchiveView? _archiveView;
+    
     public MainWindow()
     {
         InitializeComponent();
+        _youtubeView = new YoutubeView();
+        MainContent.Content = _youtubeView;
+    }
+
+    void OnClickViewYoutube( object? sender, RoutedEventArgs args )
+    {
+        MainContent.Content = _youtubeView;
+    }
+    void OnClickViewArchive( object? sender, RoutedEventArgs args )
+    {
+        _archiveView ??= new ArchiveView();
+        MainContent.Content = _archiveView;
     }
 }
